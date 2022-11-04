18fc8ef1b29443cfb9f76dc6ad5b11289dcde1ab
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-04 11:44:00 +0100

    refactor(drinks): add DrinkKindToDrinkKindApiV1Converter

---
+++ src/drinks/infrastructure/api/v1/converters/DrinkKindToDrinkKindApiV1Converter.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+import { Converter } from '../../../../../common/domain/modules/Converter';
+import { DrinkKind } from '../../../../domain/models/DrinkKind';
+import { DrinkKindApiV1 } from '../models/DrinkKindApiV1';
+
+const drinkKindToDrinkKindApiV1Map: {
+  [TKey in DrinkKind]: DrinkKindApiV1;
+} = Object.freeze({
+  [DrinkKind.alcoholic]: DrinkKindApiV1.alcoholic,
+  [DrinkKind.soda]: DrinkKindApiV1.soda,
+});
+
+@Injectable()
+export class DrinkKindToDrinkKindApiV1Converter
+  implements Converter<DrinkKind, DrinkKindApiV1>
+{
+  public convert(drinkKind: DrinkKind): DrinkKindApiV1 {
+    return drinkKindToDrinkKindApiV1Map[drinkKind];
+  }
+}
