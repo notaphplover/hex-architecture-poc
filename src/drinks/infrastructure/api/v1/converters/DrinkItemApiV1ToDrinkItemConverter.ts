9accc1e8f382ed6d52442848362d2c129b3c1efe
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-04 11:40:20 +0100

    refactor(drinks): add DrinkItemApiV1ToDrinkItemConverter

---
+++ src/drinks/infrastructure/api/v1/converters/DrinkItemApiV1ToDrinkItemConverter.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+import { Converter } from '../../../../../common/domain/modules/Converter';
+import { AppError } from '../../../../../errors/application/models/AppError';
+import { AppErrorKind } from '../../../../../errors/application/models/AppErrorKind';
+import { DrinkItem } from '../../../../domain/models/DrinkItem';
+import { Liquid } from '../../../../domain/models/Liquid';
+import { DrinkItemApiV1 } from '../models/DrinkItemApiV1';
+
+@Injectable()
+export class DrinkItemApiV1ToDrinkItemConverter
+  implements Converter<DrinkItemApiV1, DrinkItem, Liquid>
+{
+  public convert(
+    drinkItemCosmosDbSql: DrinkItemApiV1,
+    liquid: Liquid,
+  ): DrinkItem {
+    if (drinkItemCosmosDbSql.liquid_id !== liquid.id) {
+      throw new AppError(
+        AppErrorKind.unknown,
+        'Unexpected attempt to create a drink item from a liquid with different id',
+      );
+    }
+
+    return {
+      liquid: liquid,
+      milliliters: drinkItemCosmosDbSql.milliliters,
+    };
+  }
+}
