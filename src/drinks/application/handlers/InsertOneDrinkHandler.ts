18bb5cce8aa0f06fec4f0ad74205aa74bc4d9b87
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-06 21:40:29 +0100

    refactor(drinks): update InsertOneDrinkHandler to respect the drink creation rule

--- src/drinks/application/handlers/InsertOneDrinkHandler.ts
+++ src/drinks/application/handlers/InsertOneDrinkHandler.ts
@@ -2,22 +2,45 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { Handler } from '../../../common/application/modules/Handler';
 import { InsertOneEntityPort } from '../../../db/application/ports/InsertOneEntityPort';
+import { AppError } from '../../../errors/application/models/AppError';
+import { AppErrorKind } from '../../../errors/application/models/AppErrorKind';
 import { drinksInjectionSymbolsMap } from '../../domain/injection/drinksInjectionSymbolsMap';
 import { Drink } from '../../domain/models/Drink';
+import { DrinkItem } from '../../domain/models/DrinkItem';
+import { Liquid } from '../../domain/models/Liquid';
+import { DrinkCreationRuleValidator } from '../../domain/ruleValidators/DrinkCreationRuleValidator';
 import { DrinkInsertQuery } from '../queries/DrinkInsertQuery';
 
 @Injectable()
 export class InsertOneDrinkHandler implements Handler<DrinkInsertQuery, Drink> {
+  readonly #drinkCreationRuleValidator: DrinkCreationRuleValidator;
   readonly #insertOneDrinkPort: InsertOneEntityPort<DrinkInsertQuery, Drink>;
 
   constructor(
+    @Inject(drinksInjectionSymbolsMap.drinkCreationRuleValidator)
+    drinkCreationRuleValidator: DrinkCreationRuleValidator,
     @Inject(drinksInjectionSymbolsMap.insertOneDrinkAdapter)
     insertOneDrinkPort: InsertOneEntityPort<DrinkInsertQuery, Drink>,
   ) {
+    this.#drinkCreationRuleValidator = drinkCreationRuleValidator;
     this.#insertOneDrinkPort = insertOneDrinkPort;
   }
 
   public async handle(drinkInsertQuery: DrinkInsertQuery): Promise<Drink> {
-    return this.#insertOneDrinkPort.adapt(drinkInsertQuery);
+    if (
+      this.#drinkCreationRuleValidator.isValid(
+        drinkInsertQuery.kind,
+        drinkInsertQuery.items.map(
+          (drinkItem: DrinkItem): Liquid => drinkItem.liquid,
+        ),
+      )
+    ) {
+      return this.#insertOneDrinkPort.adapt(drinkInsertQuery);
+    } else {
+      throw new AppError(
+        AppErrorKind.unprocessableOperation,
+        'The drink could not be created!',
+      );
+    }
   }
 }
