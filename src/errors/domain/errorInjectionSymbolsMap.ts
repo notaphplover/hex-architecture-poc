b8c275e51f0ec466b145584682b3b4bf9cb705a3
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-07 16:22:51 +0100

    refactor(errors): remove HandleErrorAdapter

--- src/errors/domain/errorInjectionSymbolsMap.ts
+++ src/errors/domain/errorInjectionSymbolsMap.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/typedef
 export const errorInjectionSymbolsMap = {
   errorToResponseConverter: Symbol.for('ErrorToResponseConverter'),
-  handleErrorPort: Symbol.for('HandleErrorPort'),
 };
--- src/errors/infrastructure/nestJs/ErrorsContainerModule.ts
+++ src/errors/infrastructure/nestJs/ErrorsContainerModule.ts
@@ -2,21 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { ErrorToResponseConverter } from '../../application/converters/ErrorToResponseConverter';
 import { errorInjectionSymbolsMap } from '../../domain/errorInjectionSymbolsMap';
-import { HandleErrorAdapter } from '../azure/adapters/HandleErrorAdapter';
 
 @Module({
-  exports: [
-    errorInjectionSymbolsMap.errorToResponseConverter,
-    errorInjectionSymbolsMap.handleErrorPort,
-  ],
+  exports: [errorInjectionSymbolsMap.errorToResponseConverter],
   providers: [
     {
       provide: errorInjectionSymbolsMap.errorToResponseConverter,
       useClass: ErrorToResponseConverter,
-    },
-    {
-      provide: errorInjectionSymbolsMap.handleErrorPort,
-      useClass: HandleErrorAdapter,
     },
   ],
 })
