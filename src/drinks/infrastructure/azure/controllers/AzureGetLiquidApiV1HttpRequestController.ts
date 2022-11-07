f1942f0d9c4c1970f0da621214b74dbec8f4f222
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-07 15:43:16 +0100

    refactor(drinks): add AzureGetLiquidApiV1HttpRequestController

--- src/drinks/infrastructure/azure/controllers/AzureGetLiquidApiV1HttpRequestController.ts
+++ src/drinks/infrastructure/azure/controllers/AzureGetLiquidApiV1HttpRequestController.ts
@@ -1,64 +1,40 @@
 import { HttpRequest, HttpResponse } from '@azure/functions';
 import { Inject, Injectable } from '@nestjs/common';
 
-import { Port } from '../../../../common/application/modules/Port';
-import { UseCase } from '../../../../common/application/modules/UseCase';
+import { Controller } from '../../../../common/application/modules/Controller';
 import { Converter } from '../../../../common/domain/modules/Converter';
-import { errorInjectionSymbolsMap } from '../../../../errors/domain/errorInjectionSymbolsMap';
 import { Request } from '../../../../http/application/models/Request';
-import { RequestProcessor } from '../../../../http/application/modules/RequestProcessor';
-import { HttpSingleEntityResponseCreateQuery } from '../../../../http/application/query/HttpSingleEntityResponseCreateQuery';
+import { RequestWithBody } from '../../../../http/application/models/RequestWithBody';
+import { Response } from '../../../../http/application/models/Response';
+import { ResponseWithBody } from '../../../../http/application/models/ResponseWithBody';
 import { httpInjectionSymbolsMap } from '../../../../http/domain/httpInjectionSymbolsMap';
-import { AzureHttpSingleEntityRequestController } from '../../../../http/infrastructure/azure/modules/AzureHttpSingleEntityRequestController';
-import { LiquidApiV1 } from '../../../application/models/api/v1/LiquidApiV1';
-import { LiquidFindQuery } from '../../../application/queries/LiquidFindQuery';
+import { AzureRequestController } from '../../../../http/infrastructure/azure/controllers/AzureRequestController';
 import { drinksInjectionSymbolsMap } from '../../../domain/injection/drinksInjectionSymbolsMap';
-import { Liquid } from '../../../domain/models/Liquid';
 
 @Injectable()
-export class AzureGetLiquidApiV1HttpRequestController extends AzureHttpSingleEntityRequestController<
-  Request,
-  LiquidFindQuery,
-  Liquid,
-  LiquidApiV1
-> {
+export class AzureGetLiquidApiV1HttpRequestController extends AzureRequestController {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
-    @Inject(errorInjectionSymbolsMap.handleErrorPort)
-    handleErrorPort: Port<unknown, HttpResponse>,
     @Inject(httpInjectionSymbolsMap.azureHttpRequestToRequestConverter)
-    azureGetLiquidApiV1HttpRequestToRequestConverter: Converter<
+    azureHttpRequestToRequestConverter: Converter<
       HttpRequest,
-      Request
+      Request | RequestWithBody
     >,
-    @Inject(drinksInjectionSymbolsMap.getLiquidApiV1HttpRequestProcessor)
-    getLiquidApiV1HttpRequestProcessor: RequestProcessor<
-      Request,
-      LiquidFindQuery
+    @Inject(drinksInjectionSymbolsMap.getLiquidApiV1RequestController)
+    requestController: Controller<
+      Request | RequestWithBody,
+      Response | ResponseWithBody<unknown>
     >,
-    @Inject(drinksInjectionSymbolsMap.findOneLiquidUseCase)
-    insertOneLiquidUseCase: UseCase<LiquidFindQuery, Liquid>,
-    @Inject(drinksInjectionSymbolsMap.liquidToLiquidApiV1Converter)
-    liquidToLiquidApiV1Converter: Converter<Liquid, LiquidApiV1>,
-    @Inject(
-      httpInjectionSymbolsMap.httpResponseCreateQueryToGetResponseWithEntityCreatedConverter,
-    )
-    httpResponseCreateQueryToResponseConverter: Converter<
-      HttpSingleEntityResponseCreateQuery<
-        LiquidFindQuery,
-        Liquid,
-        LiquidApiV1 | undefined
-      >,
+    @Inject(httpInjectionSymbolsMap.responseToAzureHttpResponseConverter)
+    responseToAzureHttpResponseConverter: Converter<
+      Response | ResponseWithBody<unknown>,
       HttpResponse
     >,
   ) {
     super(
-      handleErrorPort,
-      azureGetLiquidApiV1HttpRequestToRequestConverter,
-      getLiquidApiV1HttpRequestProcessor,
-      insertOneLiquidUseCase,
-      liquidToLiquidApiV1Converter,
-      httpResponseCreateQueryToResponseConverter,
+      azureHttpRequestToRequestConverter,
+      requestController,
+      responseToAzureHttpResponseConverter,
     );
   }
 }
