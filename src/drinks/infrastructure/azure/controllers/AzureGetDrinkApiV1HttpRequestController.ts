fb67f2ccd6ea80f763f11928a9bb836209b489e7
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-07 15:43:01 +0100

    refactor(drinks): add AzureGetDrinkApiV1HttpRequestController

--- src/drinks/infrastructure/azure/controllers/AzureGetDrinkApiV1HttpRequestController.ts
+++ src/drinks/infrastructure/azure/controllers/AzureGetDrinkApiV1HttpRequestController.ts
@@ -1,64 +1,40 @@
 import { HttpRequest, HttpResponse } from '@azure/functions';
 import { Inject, Injectable } from '@nestjs/common';
 
-import { Port } from '../../../../common/application/modules/Port';
-import { UseCase } from '../../../../common/application/modules/UseCase';
+import { Controller } from '../../../../common/application/modules/Controller';
 import { Converter } from '../../../../common/domain/modules/Converter';
-import { errorInjectionSymbolsMap } from '../../../../errors/domain/errorInjectionSymbolsMap';
 import { Request } from '../../../../http/application/models/Request';
-import { RequestProcessor } from '../../../../http/application/modules/RequestProcessor';
-import { HttpSingleEntityResponseCreateQuery } from '../../../../http/application/query/HttpSingleEntityResponseCreateQuery';
+import { RequestWithBody } from '../../../../http/application/models/RequestWithBody';
+import { Response } from '../../../../http/application/models/Response';
+import { ResponseWithBody } from '../../../../http/application/models/ResponseWithBody';
 import { httpInjectionSymbolsMap } from '../../../../http/domain/httpInjectionSymbolsMap';
-import { AzureHttpSingleEntityRequestController } from '../../../../http/infrastructure/azure/modules/AzureHttpSingleEntityRequestController';
-import { DrinkApiV1 } from '../../../application/models/api/v1/DrinkApiV1';
-import { DrinkFindQuery } from '../../../application/queries/DrinkFindQuery';
+import { AzureRequestController } from '../../../../http/infrastructure/azure/controllers/AzureRequestController';
 import { drinksInjectionSymbolsMap } from '../../../domain/injection/drinksInjectionSymbolsMap';
-import { Drink } from '../../../domain/models/Drink';
 
 @Injectable()
-export class AzureGetDrinkApiV1HttpRequestController extends AzureHttpSingleEntityRequestController<
-  Request,
-  DrinkFindQuery,
-  Drink,
-  DrinkApiV1
-> {
+export class AzureGetDrinkApiV1HttpRequestController extends AzureRequestController {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
-    @Inject(errorInjectionSymbolsMap.handleErrorPort)
-    handleErrorPort: Port<unknown, HttpResponse>,
     @Inject(httpInjectionSymbolsMap.azureHttpRequestToRequestConverter)
-    azureGetDrinkApiV1HttpRequestToRequestConverter: Converter<
+    azureHttpRequestToRequestConverter: Converter<
       HttpRequest,
-      Request
+      Request | RequestWithBody
     >,
-    @Inject(drinksInjectionSymbolsMap.getDrinkApiV1HttpRequestProcessor)
-    getDrinkApiV1HttpRequestProcessor: RequestProcessor<
-      Request,
-      DrinkFindQuery
+    @Inject(drinksInjectionSymbolsMap.getDrinkApiV1RequestController)
+    requestController: Controller<
+      Request | RequestWithBody,
+      Response | ResponseWithBody<unknown>
     >,
-    @Inject(drinksInjectionSymbolsMap.findOneDrinkUseCase)
-    insertOneDrinkUseCase: UseCase<DrinkFindQuery, Drink>,
-    @Inject(drinksInjectionSymbolsMap.drinkToDrinkApiV1Converter)
-    drinkToDrinkApiV1Converter: Converter<Drink, DrinkApiV1>,
-    @Inject(
-      httpInjectionSymbolsMap.httpResponseCreateQueryToGetResponseWithEntityCreatedConverter,
-    )
-    httpResponseCreateQueryToResponseConverter: Converter<
-      HttpSingleEntityResponseCreateQuery<
-        DrinkFindQuery,
-        Drink,
-        DrinkApiV1 | undefined
-      >,
+    @Inject(httpInjectionSymbolsMap.responseToAzureHttpResponseConverter)
+    responseToAzureHttpResponseConverter: Converter<
+      Response | ResponseWithBody<unknown>,
       HttpResponse
     >,
   ) {
     super(
-      handleErrorPort,
-      azureGetDrinkApiV1HttpRequestToRequestConverter,
-      getDrinkApiV1HttpRequestProcessor,
-      insertOneDrinkUseCase,
-      drinkToDrinkApiV1Converter,
-      httpResponseCreateQueryToResponseConverter,
+      azureHttpRequestToRequestConverter,
+      requestController,
+      responseToAzureHttpResponseConverter,
     );
   }
 }
