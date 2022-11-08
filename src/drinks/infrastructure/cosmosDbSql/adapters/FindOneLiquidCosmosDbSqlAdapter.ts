cb8eecfaa5f043d3b211d7b235e81c3f8e225f8c
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-08 13:07:29 +0100

    refactor(drinks): update FindOneLiquidCosmosDbSqlAdapter
    add liquidFindQueryToCosmosDbSqlFeedOptionsConverter dependency

--- src/drinks/infrastructure/cosmosDbSql/adapters/FindOneLiquidCosmosDbSqlAdapter.ts
+++ src/drinks/infrastructure/cosmosDbSql/adapters/FindOneLiquidCosmosDbSqlAdapter.ts
@@ -1,4 +1,4 @@
-import { Container, SqlQuerySpec } from '@azure/cosmos';
+import { Container, FeedOptions, SqlQuerySpec } from '@azure/cosmos';
 import { Inject, Injectable } from '@nestjs/common';
 
 import { Converter } from '../../../../common/domain/modules/Converter';
@@ -41,6 +41,13 @@ export class FindOneLiquidCosmosDbSqlAdapter extends FindOneEntityCosmosDbSqlAda
       FindOneQuery<LiquidCosmosDbSql>
     >,
     @Inject(
+      drinksInjectionSymbolsMap.liquidFindQueryToCosmosDbSqlFeedOptionsConverter,
+    )
+    liquidFindQueryToCosmosDbSqlFeedOptionsConverter: Converter<
+      LiquidFindQuery,
+      FeedOptions
+    >,
+    @Inject(
       dbInjectionSymbolsMap.cosmosDbSqlFindOneQueryToCosmosDbSqlQuerySpecConverter,
     )
     cosmosDbSqlFindOneQueryToCosmosDbSqlQuerySpecConverter: Converter<
@@ -52,7 +59,7 @@ export class FindOneLiquidCosmosDbSqlAdapter extends FindOneEntityCosmosDbSqlAda
       container,
       liquidCosmosDbSqlToLiquidConverters,
       liquidFindQueryToLiquidCosmosDbSqlFindOneQueryConverter,
-      undefined,
+      liquidFindQueryToCosmosDbSqlFeedOptionsConverter,
       cosmosDbSqlFindOneQueryToCosmosDbSqlQuerySpecConverter,
     );
   }
